2af66da73e2b09331aef55bf4a00023567a808f3
fedescap02 <federicoscapellato51@gmail.com>
2022-11-15 12:01:26 -0300

    Cambios en dinamico y falta funcion kilometro en greedy

--- Properties/Class_Vehiculo.cs
+++ Properties/Class_Vehiculo.cs
@@ -25,7 +25,7 @@ namespace tp_final.Properties
         protected float Largo_Max;
         protected float Alto_Max;
         protected float DistanciaTot;
-        
+
         public int Peso_Max { get; set; }
         public int Vol_Max { get; set; }
         public int Capacidad_Tanque { get; }
@@ -38,7 +38,7 @@ namespace tp_final.Properties
         public static uint Max_ID = 0;
 
         public Dictionary<string, (string, string)> Coordenadas { get; }
-        
+
         public Class_Vehiculo(float Ancho_Max, float Largo_Max, float Alto_Max, int Peso_Max, int Capacidad_Tanque, float Consumo_Tanque)
         {
             ID = Max_ID++;
@@ -54,38 +54,84 @@ namespace tp_final.Properties
             Coordenadas = CargarCoordenadas();
             DistanciaTot = 0;
         }
-        
-        public void Recorrido()
+        public void setLista(List<Pedido> Aux)
         {
-            float min, DistanciaTot = 0;
-            for(int i = 0; i < Pedidos.Count - 1; i++)
+            this.Pedidos = Aux;
+        }
+        public void MostrarAlgo()
+        {
+            for (int i = 0; Recorrido.Count != 0; i++)
             {
-                for(int j = 0; j < Pedidos.Count - 1; j++)
-                {
-                    if(DistanciaKm() < min || i == 0)
-                    {
-                        min = DistanciaKm(coordenadas[recorrido[k]], lista - getlista()[i]);
-                        int k = 1;
-                        recorrido[k] = aux->listaProd->getLista()[i];
-                        k++;
-                        aux->listaProd->Eliminar(i);
-                    }
-                }
-                DistanciaTot += min;
+                Console.Write(Recorrido.Dequeue());
+                Console.WriteLine();
             }
-
+        }
+        public void CargarRecorrido()
+        {
+            List<string> Nodos = new List<string>();
+            Nodos = FiltrarRepetidos();
+            string Inicial = "Liniers";
+            while (Nodos.Count > 0)
+            {
+                Recorrido.Enqueue(Inicial);
+                Nodos.Remove(Inicial);
+                Inicial = NodosMasCercano(Inicial, Nodos);
+            }
+            Recorrido.Enqueue("Liniers");
+            DistanciaTot += DistanciaKm(Inicial, "Liniers");
+        }
+        public string NodosMasCercano(string Inicial, List<string> Nodos)
+        {
+            float min = 0;
+            string aux = "error";
+            for (int i = 0; i < Nodos.Count; i++)
+            {
+                if (DistanciaKm(Inicial, Nodos[i]) < min || i == 0)
+                {
+                    min = DistanciaKm(Inicial, Nodos[i]);
+                    aux = Nodos[i];
+                }
+            }
+            DistanciaTot += min;
+            return aux;
+        }
+        public List<string> FiltrarRepetidos()
+        {
+            HashSet<string> Aux_No_Repetidos = new HashSet<string>();
+            for (int i = 0; i < Pedidos.Count; i++)
+            {
+                Aux_No_Repetidos.Add(Pedidos[i].barrio);//si esta repetido no te lo agrega, funciona como un dictionary las claves no se repiten
+            }
+            return Aux_No_Repetidos.ToList<string>();
         }
 
-        public static float DistanciaKm()
+        public Dictionary<string, (string, string)> CargarCoordenadas()
         {
-            
+            Dictionary<string, (string, string)> Coordenadas = new Dictionary<string, (string, string)>();
+            string ubicacionArchivo = "D:\\Repos\\TP_Final_Grupo_2\\Coordenadas.csv";
+            System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacionArchivo);
+            string separador = ",";
+            string linea;
+
+            // Si el archivo no tiene encabezado, elimina la siguiente línea
+            archivo.ReadLine(); // Leer la primera línea pero descartarla porque es el encabezado
+            while ((linea = archivo.ReadLine()) != null)
+            {
+                string[] fila = linea.Split(separador);
+                Coordenadas.Add(fila[0], (fila[1], fila[2]));
+            }
+            return Coordenadas;
+        }
+        public static float DistanciaKm(string Origen, string Destino)
+        {
+
             var random = new Random();
 
             var value = random.Next(1, 100000);
             /*float RadTierra = 6378.0F;
 
-        //    var difLatitud = (posDestino.Latitud – posOrigen.Latitud).EnRadianes();
-        //    var difLongitud = (posDestino.Longitud - posOrigen.Longitud).EnRadianes();
+            var difLatitud = (posDestino.Latitud – posOrigen.Latitud).EnRadianes();
+            var difLongitud = (posDestino.Longitud - posOrigen.Longitud).EnRadianes();
 
 
             var a = Math.Sin(difLatitud / 2).AlCuadrado() +
@@ -96,5 +142,6 @@ namespace tp_final.Properties
             return RadTierra * c;*/
             return value;
         }
+
     }
 }
